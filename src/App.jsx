aa1429c9c1120885f3eb7e09631ff66f142f75c6
dhruv-1108 <dhruv1108aggarwal@gmail.com>
2022-06-27 17:31:48 +0530

    Added game history

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Board from "./components/Board";
+import History from "./components/History";
 import { calculateWinner } from "./WinnerCalculator";
 
 import './styles/root.scss'
@@ -41,6 +42,10 @@ const App = () => {
     setCurrentMove(prev => prev + 1)
   };
 
+  const moveTo = (move) => {
+     setCurrentMove(move); 
+  }
+
   return (
     <div className="app">
       <h2>
@@ -48,6 +53,8 @@ const App = () => {
       </h2>
       <h2>{ message }</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <br></br>
+      <History history={history} moveTo = { moveTo } currentMove = { currentMove } />
     </div>
   )
 }
